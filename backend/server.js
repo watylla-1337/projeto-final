8d20acd46ee90e24fc7e9a8c1d02dc31ff6cdeca
Watylla Macedo <83824090+watylla-1337@users.noreply.github.com>
2023-12-01 10:01:24 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -9,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://watyllamacedo:<passoword>@watylla.g2kbtrd.mongodb.net/";
+const uri = "mongodb+srv://watyllamacedo:<password>@watylla.g2kbtrd.mongodb.net/";
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
@@ -26,22 +25,66 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
+// Registration endpoint
 app.post('/register', async (req, res) => {
   const { username, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
   const user = new User({ username, password: hashedPassword });
-  await user.save();
-  res.send('User registered successfully!');
+  try {
+    await user.save();
+    res.status(201).send('User registered successfully!');
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
+
 
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
   const user = await User.findOne({ username });
   if (user && await bcrypt.compare(password, user.password)) {
-    res.send('Login successful!');
+    res.status(200).send('Login successful!');
   } else {
-    res.send('Invalid username or password.');
+    res.status(401).send('Invalid username or password.');
   }
 });
 
+
+app.get('/users', async (req, res) => {
+  const users = await User.find();
+  res.send(users.map(user => ({ username: user.username, id: user._id })));
+});
+
+
+app.get('/users/:id', async (req, res) => {
+  const user = await User.findById(req.params.id, 'username');
+  if (user) {
+    res.send(user);
+  } else {
+    res.status(404).send('User not found');
+  }
+});
+
+
+app.put('/users/:id', async (req, res) => {
+  const { username } = req.body;
+  const user = await User.findByIdAndUpdate(req.params.id, { username }, { new: true });
+  if (user) {
+    res.send(user);
+  } else {
+    res.status(404).send('User not found');
+  }
+});
+
+
+app.delete('/users/:id', async (req, res) => {
+  const result = await User.findByIdAndDelete(req.params.id);
+  if (result) {
+    res.send({ message: 'User deleted successfully' });
+  } else {
+    res.status(404).send('User not found');
+  }
+});
+
+// Start server
 app.listen(3000, () => console.log('Server running on port 3000'));
